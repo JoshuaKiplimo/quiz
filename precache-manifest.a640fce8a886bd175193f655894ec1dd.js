5d5f6eccd19276a73a6042a85b2dc76a9a427dee
JoshuaKiplimo <joshuakiprono8@gmail.com>
2022-10-02 22:17:34 -0400

    Updates

---
+++ precache-manifest.a640fce8a886bd175193f655894ec1dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a59d69a01a8288c56b00a6d442ecb49",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "eec8348ca25e4df44877",
+    "url": "/quiz/static/css/main.35088b67.chunk.css"
+  },
+  {
+    "revision": "13e196a7d3b1bec2a35d",
+    "url": "/quiz/static/js/2.253d9a6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz/static/js/2.253d9a6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eec8348ca25e4df44877",
+    "url": "/quiz/static/js/main.da393f27.chunk.js"
+  },
+  {
+    "revision": "dd19b6e4ede99b84197b",
+    "url": "/quiz/static/js/runtime-main.7a6c2dfa.js"
+  }
+]);
